25f22e39ba0f6dc31f227daa63ab62d4819c911b
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-09-26 21:52:27 -0300

    Add ContactsNav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ContactsNav from './components/ContactsNav/ContactsNav';
 import TopNav from './components/TopNav/TopNav';
 
 function App() {
   return (
     <div className="font-manrope">
       <TopNav />
+      <ContactsNav />
     </div>
   );
 }
---
+++ src/components/ContactsNav/ContactsNav.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import AvatarTwo from '../../assets/avatar2.png';
+import AvatarThree from '../../assets/avatar3.png';
+import AvatarFour from '../../assets/avatar4.png';
+import AvatarFive from '../../assets/avatar5.png';
+import plusButton from '../../assets/plusButton.svg';
+
+export default function ContactsNav() {
+	return (
+		<div className='flex bg-zinc-900 py-2 px-4 gap-4 justify-center'>
+			<div className='flex gap-4 overflow-x-auto'>
+				<img src={AvatarTwo} alt="" />
+				<img src={AvatarThree} alt="" />
+				<img src={AvatarFour} alt="" />
+				<img src={AvatarFive} alt="" />
+				<img src={AvatarTwo} alt="" />
+				<img src={AvatarThree} alt="" />
+				<img src={AvatarFour} alt="" />
+				<img src={AvatarFive} alt="" />
+			</div>
+
+			<button className='bg-zinc-700 p-3 rounded-full flex items-center justify-center'>
+				<img src={plusButton} alt="" />
+				</button>
+		</div>
+	)
+}
