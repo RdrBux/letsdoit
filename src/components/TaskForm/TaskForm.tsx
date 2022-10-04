cea92d3c7448b14ba71d7095b3832b79375a8d4d
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-10-04 20:27:14 -0300

    Make controlled inputs on TaskForm

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+import React, { useState } from 'react';
 import OutsideAlerter from '../OutsideAlerter/OutsideAlerter';
 
 type Props = {
@@ -5,6 +7,20 @@ type Props = {
 };
 
 export default function TaskForm({ close }: Props) {
+  const [title, setTitle] = useState('título');
+  const [description, setDescription] = useState('descripción');
+  const [date, setDate] = useState(format(new Date(), 'yyyy-MM-dd'));
+  const [hour, setHour] = useState(format(new Date(), 'HH:mm'));
+
+  function handleSubmitForm(e: React.BaseSyntheticEvent) {
+    e.preventDefault();
+
+    setTitle('');
+    setDescription('');
+    setDate(format(new Date(), 'yyyy-MM-dd'));
+    setHour(format(new Date(), 'HH:mm'));
+  }
+
   return (
     <div className="absolute inset-0 w-screen h-screen z-50 bg-zinc-900/80 flex items-center justify-center">
       <OutsideAlerter action={close}>
@@ -26,19 +42,48 @@ export default function TaskForm({ close }: Props) {
             </svg>
           </button>
           <h1 className="text-lg font-bold">AGREGAR EVENTO</h1>
-          <form className="font-bold text-sm text-zinc-600 mt-4 flex flex-col gap-3">
+          <form
+            onSubmit={handleSubmitForm}
+            className="font-bold text-sm text-zinc-600 mt-4 flex flex-col gap-3"
+          >
             <label className="flex flex-col gap-2">
               Título
-              <input className="h-8 pl-2 border" type="text" />
+              <input
+                className="h-8 pl-2 border"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </label>
             <label className="flex flex-col gap-2">
               Descripción
-              <input className="h-8 pl-2 border" type="text" />
+              <input
+                className="h-8 pl-2 border"
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
             </label>
-            <label className="flex flex-col gap-2">
-              Fecha y hora
-              <input className="h-8 pl-2 border" type="datetime-local" />
-            </label>
+            <div className="flex gap-4">
+              <label className="flex flex-col gap-2">
+                Fecha
+                <input
+                  className="h-8 pl-2 border"
+                  type="date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </label>
+              <label className="flex flex-col gap-2">
+                Hora
+                <input
+                  className="h-8 pl-2 border"
+                  type="time"
+                  value={hour}
+                  onChange={(e) => setHour(e.target.value)}
+                />
+              </label>
+            </div>
             <label className="flex flex-col gap-2">
               Participantes
               <input
