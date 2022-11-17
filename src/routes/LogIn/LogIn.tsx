e1b1b0fc6af63406f444f11f3d6c27e1b4794c2c
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-11-17 19:32:05 -0300

    Change login route

--- src/routes/LogIn/LogIn.tsx
+++ src/routes/LogIn/LogIn.tsx
@@ -2,9 +2,9 @@ import { signInWithRedirect } from 'firebase/auth';
 import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
-import { auth, googleProvider, facebookProvider } from '../../firebase';
+import { auth, googleProvider } from '../../firebase';
 import googleLogo from '../../assets/googleLogo.svg';
-import facebookLogo from '../../assets/facebookLogo.svg';
+import logInImage from '../../assets/login-img.svg';
 
 export default function LogIn() {
   const user = useContext(AuthContext);
@@ -17,36 +17,40 @@ export default function LogIn() {
     }
   }
 
-  async function handleLogInFacebook() {
-    /* try {
-      await signInWithRedirect(auth, facebookProvider);
-    } catch (err) {
-      console.log(err);
-    } */
-    console.log('hi');
-  }
-
   return (
-    <div className="login-bg | flex h-screen flex-col items-center">
-      {user.id && <Navigate to="/" replace={true} />}
-      <h1 className="my-12 text-5xl">DOIT</h1>
-      <div className="flex flex-col gap-2 rounded bg-white p-6 text-zinc-700">
-        <p className="mb-2">Iniciar sesión con</p>
-        <div className="flex gap-4">
-          <button
-            className="flex items-center gap-2 rounded border-2 border-zinc-200 py-3 px-4 hover:bg-zinc-100"
-            onClick={handleLogInGoogle}
-          >
-            <img className="w-6" src={googleLogo} alt="" />
-            <div>Google</div>
-          </button>
-          <button
-            className="flex items-center gap-2 rounded border-2 border-zinc-200 py-3 px-4 hover:bg-zinc-100"
-            onClick={handleLogInFacebook}
-          >
-            <img className="w-6" src={facebookLogo} alt="" />
-            <div>Facebook</div>
-          </button>
+    <div className="flex min-h-screen flex-col items-center bg-emerald-50 p-2 pb-6 font-manrope">
+      <div className="container mt-6 flex flex-col-reverse items-center  lg:mt-10 lg:flex-row">
+        {user.id && <Navigate to="/" replace={true} />}
+        <div className="flex flex-col gap-2 rounded bg-white p-4 shadow-lg lg:w-7/12 lg:gap-4 lg:p-10">
+          <h1 className="text-3xl font-semibold lg:text-5xl">
+            El siguiente gran paso para lograr todas tus metas
+          </h1>
+          <div className="text-zinc-700">
+            DO IT es el calendario virtual que te organizará tu vida personal y
+            social. Puedes agregar amigos, compartir actividades y chatear con
+            ellos desde la aplicación.
+          </div>
+          <div className="flex w-fit flex-col gap-4 rounded border border-black px-6 py-4 text-black shadow-lg">
+            <p className="font-semibold">Ingresar:</p>
+            <button
+              className="flex items-center gap-2 rounded border border-black bg-white py-3 px-6 shadow hover:bg-zinc-100 lg:px-20"
+              onClick={handleLogInGoogle}
+            >
+              <img className="w-6" src={googleLogo} alt="" />
+              <div className="font-medium">Continuar con Google</div>
+            </button>
+            <div className="relative w-full border">
+              <div className="absolute -top-3 left-1/2 -translate-x-1/2 bg-white px-2">
+                o
+              </div>
+            </div>
+            <div className="font-medium underline">
+              Usar una cuenta de prueba.
+            </div>
+          </div>
+        </div>
+        <div className="-mb-24 lg:mb-0">
+          <img src={logInImage} alt="" />
         </div>
       </div>
     </div>
