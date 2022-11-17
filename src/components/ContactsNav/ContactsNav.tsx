c385884407a16caa890f38b2488f7a1922a91204
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-11-17 17:28:02 -0300

    Change search and contacts icons

--- src/components/ContactsNav/ContactsNav.tsx
+++ src/components/ContactsNav/ContactsNav.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
-import msgIcon from '../../assets/msgIcon.svg';
 import { NotifContext } from '../../context/NotifContext';
 import { FriendData, SelectedUser } from '../../types/types';
 
@@ -55,9 +54,16 @@ export default function ContactsNav({
 
       <button
         onClick={openMenu}
-        className="flex h-10 w-10 items-center justify-center rounded-full bg-black/25 p-3"
+        className="flex h-10 w-10 items-center justify-center rounded-full bg-black/25 p-2 text-white"
       >
-        <img src={msgIcon} alt="" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+        </svg>
       </button>
     </div>
   );
